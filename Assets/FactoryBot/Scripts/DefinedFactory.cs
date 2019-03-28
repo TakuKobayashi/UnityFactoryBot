06ffd6f66aa947682fd6484014569607b835a361
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-03-28 18:33:53 +0900

    add feature FactoryBot list and Define

---
+++ Assets/FactoryBot/Scripts/DefinedFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+namespace FactoryBot{
+    public class DefinedFactory<T>
+    {
+        private Dictionary<string, object> instanceParams = new Dictionary<string, object>();
+
+        internal DefinedFactory(Dictionary<string, object> instanceParams){
+            this.instanceParams = instanceParams;
+        }
+
+        public T Build(Dictionary<string, object> overrideParams = default(Dictionary<string, object>))
+        {
+            this.instanceParams = this.MergeDictionary(overrideParams);
+            return Factory.Build<T>(this.instanceParams);
+        }
+
+        public List<T> BuildList(Dictionary<string, object> instanceParams = default(Dictionary<string, object>), int count = 1)
+        {
+            this.instanceParams = this.MergeDictionary(overrideParams);
+            return Factory.BuildList<T>(this.instanceParams, count);
+        }
+
+        private Dictionary<string, object> MergeDictionary(Dictionary<string, object> overrideParams = default(Dictionary<string, object>)){
+            Dictionary<string, object> mergedInstanceParams = new Dictionary<string, object>(this.instanceParams);
+            foreach (KeyValuePair<string, object> p in overrideParams)
+            {
+                if(mergedInstanceParams.ContainKey(p.Key)){
+                    mergedInstanceParams[p.Key] = p.Value;
+                }else{
+                    mergedInstanceParams.Add(p.Key, p.Value);
+                }
+            }
+            return mergedInstanceParams;
+        }
+    }
+}
---
+++ Assets/FactoryBot/Scripts/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+namespace FactoryBot{
+    public class Factory
+    {
+        public static DefinedFactory<T> Define<T>(Dictionary<string, object> instanceParams = default(Dictionary<string, object>))
+        {
+            return new DefinedFactory(instanceParams);
+        }
+
+        public static T Build<T>(Dictionary<string, object> instanceParams = default(Dictionary<string, object>))
+        {
+            Type t = typeof(T);
+            T instance = (T)Activator.CreateInstance(t);
+            FieldInfo[] fields = t.GetFields();
+            foreach (KeyValuePair<string, object> p in instanceParams)
+            {
+                FieldInfo info = Array.Find(fields, (f) => f.Name == p.Key);
+                info.SetValue(instance, p.Value);
+            }
+            return instance;
+        }
+
+        public static List<T> BuildList<T>(Dictionary<string, object> instanceParams = default(Dictionary<string, object>), int count = 1)
+        {
+            List<T> instanceList = new List<T>();
+            for (int i = 0;i < count;++i)
+            {
+                T instance = Build<T>(instanceParams);
+                instanceList.Add(instance);
+            }
+            return instanceList;
+        }
+    }
+}
