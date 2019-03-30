5ce09d3074b6cf6e1e89f802114c67fc2ffb1e45
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-03-31 02:23:33 +0900

    bugfix: don't override instance field

--- Assets/FactoryBot/Scripts/DefinedFactory.cs
+++ Assets/FactoryBot/Scripts/DefinedFactory.cs
@@ -11,19 +11,23 @@ namespace FactoryBot{
 
         public T Build(Dictionary<string, object> overrideParams = default(Dictionary<string, object>))
         {
-            this.instanceParams = this.MergeDictionary(overrideParams);
-            return Factory.Build<T>(this.instanceParams);
+            Dictionary<string, object> defiedParams = this.MergeDictionary(overrideParams);
+            return Factory.Build<T>(defiedParams);
         }
 
         public List<T> BuildList(Dictionary<string, object> overrideParams = default(Dictionary<string, object>), int count = 1)
         {
-            this.instanceParams = this.MergeDictionary(overrideParams);
-            return Factory.BuildList<T>(this.instanceParams, count);
+            Dictionary<string, object> defiedParams = this.MergeDictionary(overrideParams);
+            return Factory.BuildList<T>(defiedParams, count);
         }
 
         private Dictionary<string, object> MergeDictionary(Dictionary<string, object> overrideParams = default(Dictionary<string, object>)){
+            Dictionary<string, object> overrideDictionary = new Dictionary<string, object>();
+            if(overrideParams != null){
+                overrideDictionary = overrideParams;
+            }
             Dictionary<string, object> mergedInstanceParams = new Dictionary<string, object>(this.instanceParams);
-            foreach (KeyValuePair<string, object> p in overrideParams)
+            foreach (KeyValuePair<string, object> p in overrideDictionary)
             {
                 if(mergedInstanceParams.ContainsKey(p.Key)){
                     mergedInstanceParams[p.Key] = p.Value;
