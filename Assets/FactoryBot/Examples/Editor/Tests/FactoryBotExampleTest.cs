35a61bbb7b24c4283cb71d3b1fe59cae81549784
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-03-31 02:24:17 +0900

    add TestCase

--- Assets/FactoryBot/Examples/Editor/Tests/FactoryBotExampleTest.cs
+++ Assets/FactoryBot/Examples/Editor/Tests/FactoryBotExampleTest.cs
@@ -63,5 +63,57 @@ namespace FactoryBotExample
             Assert.AreEqual(location.lat, 0);
             Assert.AreEqual(location.lng, 0);
         }
+
+        [Test]
+        public void FactoryBotDefineBuildItemTest()
+        {
+            DefinedFactory<Item> definedItem = Factory.Define<Item>(new Dictionary<string, object>() {
+                { "name", "test" },
+                { "posX", 0.3f },
+                { "posY", 0.1f },
+                { "posZ", 0.2f },
+                { "amount", 3 }
+            });
+            Item item = definedItem.Build();
+            Assert.AreEqual(item.name, "test");
+            Assert.AreEqual(item.posX, 0.3f);
+            Assert.AreEqual(item.posY, 0.1f);
+            Assert.AreEqual(item.posZ, 0.2f);
+            Assert.AreEqual(item.amount, 3);
+
+            Item overrideItem = definedItem.Build(new Dictionary<string, object>() {
+                { "name", "testtest" },
+                { "amount", 6 }
+            });
+            Assert.AreEqual(overrideItem.name, "testtest");
+            Assert.AreEqual(overrideItem.posX, 0.3f);
+            Assert.AreEqual(overrideItem.posY, 0.1f);
+            Assert.AreEqual(overrideItem.posZ, 0.2f);
+            Assert.AreEqual(overrideItem.amount, 6);
+        }
+
+        [Test]
+        public void FactoryBotDefineBuildPertialItemTest()
+        {
+            DefinedFactory<Item> definedItem = Factory.Define<Item>(new Dictionary<string, object>() {
+                { "name", "test" },
+            });
+            Item item = definedItem.Build();
+            Assert.AreEqual(item.name, "test");
+            Assert.AreEqual(item.posX, 0f);
+            Assert.AreEqual(item.posY, 0f);
+            Assert.AreEqual(item.posZ, 0f);
+            Assert.AreEqual(item.amount, 0);
+
+            Item overrideItem = definedItem.Build(new Dictionary<string, object>() {
+                { "name", "testtest" },
+                { "amount", 6 }
+            });
+            Assert.AreEqual(overrideItem.name, "testtest");
+            Assert.AreEqual(overrideItem.posX, 0f);
+            Assert.AreEqual(overrideItem.posY, 0f);
+            Assert.AreEqual(overrideItem.posZ, 0f);
+            Assert.AreEqual(overrideItem.amount, 6);
+        }
     }
 }
