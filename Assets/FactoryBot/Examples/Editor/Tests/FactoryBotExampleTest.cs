2d5c447706be357998262211200db83468f629c2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-03-31 01:00:32 +0900

    add TestCase

--- Assets/FactoryBot/Examples/Editor/Tests/FactoryBotExampleTest.cs
+++ Assets/FactoryBot/Examples/Editor/Tests/FactoryBotExampleTest.cs
@@ -26,5 +26,42 @@ namespace FactoryBotExample
             Assert.AreEqual(item.posZ, 0.2f);
             Assert.AreEqual(item.amount, 3);
         }
+
+        [Test]
+        public void FactoryBotBuildPertialItemTest()
+        {
+            Item item = Factory.Build<Item>(new Dictionary<string, object>() {
+                { "name", "test" },
+            });
+            Assert.AreEqual(item.name, "test");
+            Assert.AreEqual(item.posX, 0f);
+            Assert.AreEqual(item.posY, 0f);
+            Assert.AreEqual(item.posZ, 0f);
+            Assert.AreEqual(item.amount, 0);
+        }
+
+        [Test]
+        public void FactoryBotBuildLocationTest()
+        {
+            Location location = Factory.Build<Location>(new Dictionary<string, object>() {
+                { "name", "test" },
+                { "lat", 0.4 },
+                { "lng", 0.5 },
+            });
+            Assert.AreEqual(location.name, "test");
+            Assert.AreEqual(location.lat, 0.4);
+            Assert.AreEqual(location.lng, 0.5);
+        }
+
+        [Test]
+        public void FactoryBotBuildPertialLocationTest()
+        {
+            Location location = Factory.Build<Location>(new Dictionary<string, object>() {
+                { "name", "test" },
+            });
+            Assert.AreEqual(location.name, "test");
+            Assert.AreEqual(location.lat, 0);
+            Assert.AreEqual(location.lng, 0);
+        }
     }
 }
